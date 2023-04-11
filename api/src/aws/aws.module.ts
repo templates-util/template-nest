2f63a942f4f415481e241839c2481687565a2b59
Renan Ribeiro <renan.lagee@gmail.com>
2023-04-11 06:56:56 -0300

    correção de test

--- api/src/aws/aws.module.ts
+++ api/src/aws/aws.module.ts
@@ -1,12 +1,13 @@
 // src/aws/aws.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Arquivo } from '../models/arquivo.entity';
 import { AwsController } from './aws.controller';
 import { S3Service } from './s3.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Arquivo])],
+  imports: [TypeOrmModule.forFeature([Arquivo]), ConfigModule],
   controllers: [AwsController],
   providers: [S3Service],
   exports: [S3Service],
